285b29fbf43c1f0c322414337c92927fd2d6fb11
BrandedNomad <info@brandednomad.com>
2020-10-18 19:04:28 +0700

    feat: styled the login view.

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,8 +7,21 @@ class Login extends Component {
 
     render() {
         return (
-            <div>
-                {this.props.users.sarahedo && <LoginSelection/>}
+            <div className='login-container'>
+                {this.props.users.sarahedo && <div className='login-box'>
+                    <div className='login-box_header'>
+                        <p className='login-box_header_title'>Welcome to the Would You Rather? app!</p>
+                        <p className='login-box_header_subtitle'>Please sign in to continue</p>
+                    </div>
+                    <img
+                        alt='would you rater'
+                        src='https://www.bbriver.com/market/image/cache/sellers/360/512-4-850x500.png'
+                        className='login-box_image'
+                    />
+                    <p className='login-box_body-text'>Login</p>
+                    <LoginSelection/>
+                </div>}
+
             </div>
         )
     }
